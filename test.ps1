52c5d35d6739b975fe8e51129f6485caefee4961
Ben Fox <benjamin.fox@xero.com>
2021-08-04 14:04:15 +1200

    Use Verbosity detailed rather than enumerating the Zip

--- test.ps1
+++ test.ps1
@@ -1,26 +1,10 @@
-[void] [Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem')
-
 function TestNuget($Version) {
     Write-Host "Testing nuget.exe $Version..." -Foreground Blue
 
     $NuGetExePath = [IO.Path]::Combine($PSScriptRoot, "tools", $Version, "nuget.exe")
     $NuSpecPath = [IO.Path]::Combine($PSScriptRoot, "example.nuspec")
     $OutputPath = [IO.Path]::Combine($PSScriptRoot, "output", $Version)
-    & $NuGetExePath pack $NuSpecPath -OutputDirectory $OutputPath
-
-    $NuPkgPath = [IO.Path]::Combine($OutputPath, "Example.0.0.0.nupkg")
-    Write-Host "Enumerating files in $NuPkgPath"
-    try {
-        $ZipFile = [IO.Compression.ZipFile]::OpenRead($NuPkgPath)
-        foreach ($Entry in $ZipFile.Entries) {
-            Write-Host $Entry.FullName -Foreground Green
-        }
-    }
-    finally {
-        if ($ZipFile) {
-            $ZipFile.Dispose()
-        }
-    }
+    & $NuGetExePath pack $NuSpecPath -OutputDirectory $OutputPath -Verbosity detailed
 }
 
 TestNuget "4.9.4"
